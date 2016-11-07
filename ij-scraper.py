b0e0dc94f9fd8271f7169c345a61272a68a49dd7
Lorenz Gorse <lorenz.gorse@gmail.com>
2016-11-07 12:35:20 +0100

    List pages not relative URLs

--- ij-scraper.py
+++ ij-scraper.py
@@ -40,50 +40,50 @@ def extract_text(tag):
 	else:
 		return tag.string.strip()
 
-urls = [
-	'/david-foster-wallace/index.php?title=Pages_3-27',
-	'/david-foster-wallace/index.php?title=Pages_27-63',
-	'/david-foster-wallace/index.php?title=Pages_63-87',
-	'/david-foster-wallace/index.php?title=Pages_87-127',
-	'/david-foster-wallace/index.php?title=Pages_127-156',
-	'/david-foster-wallace/index.php?title=Pages_157-181',
-	'/david-foster-wallace/index.php?title=Pages_181-198',
-	'/david-foster-wallace/index.php?title=Pages_198-219',
-	'/david-foster-wallace/index.php?title=Pages_219-258',
-	'/david-foster-wallace/index.php?title=Pages_258-283',
-	'/david-foster-wallace/index.php?title=Pages_283-306',
-	'/david-foster-wallace/index.php?title=Pages_306-321',
-	'/david-foster-wallace/index.php?title=Pages_321-342',
-	'/david-foster-wallace/index.php?title=Pages_343-379',
-	'/david-foster-wallace/index.php?title=Pages_380-398',
-	'/david-foster-wallace/index.php?title=Pages_398-418',
-	'/david-foster-wallace/index.php?title=Pages_418-442',
-	'/david-foster-wallace/index.php?title=Pages_442-469',
-	'/david-foster-wallace/index.php?title=Pages_470-489',
-	'/david-foster-wallace/index.php?title=Pages_489-508',
-	'/david-foster-wallace/index.php?title=Pages_508-530',
-	'/david-foster-wallace/index.php?title=Pages_531-562',
-	'/david-foster-wallace/index.php?title=Pages_563-588',
-	'/david-foster-wallace/index.php?title=Pages_589-619',
-	'/david-foster-wallace/index.php?title=Pages_620-651',
-	'/david-foster-wallace/index.php?title=Pages_651-662',
-	'/david-foster-wallace/index.php?title=Pages_663-686',
-	'/david-foster-wallace/index.php?title=Pages_686-698',
-	'/david-foster-wallace/index.php?title=Pages_698-716',
-	'/david-foster-wallace/index.php?title=Pages_716-735',
-	'/david-foster-wallace/index.php?title=Pages_736-755',
-	'/david-foster-wallace/index.php?title=Pages_755-785',
-	'/david-foster-wallace/index.php?title=Pages_785-808',
-	'/david-foster-wallace/index.php?title=Pages_809-827',
-	'/david-foster-wallace/index.php?title=Pages_827-845',
-	'/david-foster-wallace/index.php?title=Pages_845-876',
-	'/david-foster-wallace/index.php?title=Pages_876-883',
-	'/david-foster-wallace/index.php?title=Pages_883-902',
-	'/david-foster-wallace/index.php?title=Pages_902-916',
-	'/david-foster-wallace/index.php?title=Pages_916-934',
-	'/david-foster-wallace/index.php?title=Pages_934-964',
-	'/david-foster-wallace/index.php?title=Pages_964-981',
-	'/david-foster-wallace/index.php?title=Notes_and_Errata_-_Pages_983-1079']
+pages = [
+	'Pages_3-27',
+	'Pages_27-63',
+	'Pages_63-87',
+	'Pages_87-127',
+	'Pages_127-156',
+	'Pages_157-181',
+	'Pages_181-198',
+	'Pages_198-219',
+	'Pages_219-258',
+	'Pages_258-283',
+	'Pages_283-306',
+	'Pages_306-321',
+	'Pages_321-342',
+	'Pages_343-379',
+	'Pages_380-398',
+	'Pages_398-418',
+	'Pages_418-442',
+	'Pages_442-469',
+	'Pages_470-489',
+	'Pages_489-508',
+	'Pages_508-530',
+	'Pages_531-562',
+	'Pages_563-588',
+	'Pages_589-619',
+	'Pages_620-651',
+	'Pages_651-662',
+	'Pages_663-686',
+	'Pages_686-698',
+	'Pages_698-716',
+	'Pages_716-735',
+	'Pages_736-755',
+	'Pages_755-785',
+	'Pages_785-808',
+	'Pages_809-827',
+	'Pages_827-845',
+	'Pages_845-876',
+	'Pages_876-883',
+	'Pages_883-902',
+	'Pages_902-916',
+	'Pages_916-934',
+	'Pages_934-964',
+	'Pages_964-981',
+	'Notes_and_Errata_-_Pages_983-1079']
 
 html = Element('html', {
 	'xmlns:idx': 'www.mobipocket.com',
@@ -94,8 +94,8 @@ html = Element('html', {
 body = Element('body')
 html.append(body)
 
-for url in urls:
-	for d in process_page('http://infinitejest.wallacewiki.com' + url):
+for page in pages:
+	for d in process_page('http://infinitejest.wallacewiki.com/david-foster-wallace/index.php?title=' + page):
 		idx_el = SubElement(body, 'idx:entry', { 'name': 'word', 'scriptable': 'yes' })
 		SubElement(idx_el, 'idx:orth', { 'value': d[0] })
 		for part in re.split(r'\W+', d[0]):
