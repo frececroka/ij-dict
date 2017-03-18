c11e999ba784331464c777c3de93dd8069b7f0cd
Lorenz Gorse <lorenz.gorse@gmail.com>
2017-03-18 13:35:29 +0100

    Let KindleGen wrapper download KindleGen by itself.

--- kindlegen-wrapper.sh
+++ kindlegen-wrapper.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-./kindlegen "$@"; EC=$?;
+KINDLEGEN_DIR=".kindlegen"
+
+if [ ! -d $KINDLEGEN_DIR ]; then
+	curl 'http://kindlegen.s3.amazonaws.com/kindlegen_linux_2.6_i386_v2_9.tar.gz' > kindlegen.tar.gz;
+	mkdir $KINDLEGEN_DIR;
+	tar xzf kindlegen.tar.gz -C $KINDLEGEN_DIR;
+	rm kindlegen.tar.gz;
+fi
+
+./$KINDLEGEN_DIR/kindlegen "$@"; EC=$?;
 if [ $EC -eq 1 ]; then
 	exit 0;
 else
